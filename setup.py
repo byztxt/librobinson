5d4f6237261ef6fd8330e3614c6d1f44c01c04ca
Ulrik Sandborg-Petersen <ulrikp@scripturesys.com>
2017-05-04 18:17:46 +0200

    Bump to 0.2.1.

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 setup(
     name="librobinson",
-    version="0.2.0",
+    version="0.2.1",
     packages=find_packages(),
     scripts=[
         'robinson/booknames.py',
